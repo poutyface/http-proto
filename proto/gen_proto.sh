42bf32e6b2144f38df2f363f5195a5f6d369b6f1
tom <tom@tom.local>
2021-01-17 20:30:29 +0900

    generate rust lib.rs from proto

--- proto/gen_proto.sh
+++ proto/gen_proto.sh
@@ -3,6 +3,12 @@
 # for rust
 echo "generate rust code from .proto"
 protoc -I./src --rust_out=./src src/*.proto
+proto_list="./src/*.proto"
+rm -fr ./src/lib.rs
+touch ./src/lib.rs
+for file in ${proto_list[@]}; do
+    echo "pub mod `basename ${file} .proto`;" >> ./src/lib.rs
+done 
 
 # for js
 echo "generate proto_bundle.json"
